2ab7b91c70d03deab748924bf5580a514de641f6
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2024-11-18 21:52:06 -0300

    refactor

--- RentInventoryApp/app/src/main/java/com/maurosergiorodriguez/rentinventoryapp/inventorylist/ui/InventoryListScreen.kt
+++ RentInventoryApp/app/src/main/java/com/maurosergiorodriguez/rentinventoryapp/inventorylist/ui/InventoryListScreen.kt
@@ -1,16 +1,13 @@
 package com.maurosergiorodriguez.rentinventoryapp.inventorylist.ui
 
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Card
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,17 +18,13 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.maurosergiorodriguez.rentinventoryapp.R
 import com.maurosergiorodriguez.rentinventoryapp.inventorylist.ui.model.ItemModel
 import kotlinx.coroutines.CoroutineScope
@@ -82,7 +75,7 @@ fun InventoryListScreen() {
             topBar = {
                 TopAppBar(
                     title = {
-                        Text("Rent Inventory")
+                        Text(stringResource(R.string.app_name))
                     },
                     navigationIcon = {
                         IconButton(onClick = {
